26126ce0d8e2c3ad25e0d1f24e993f3296dcc77d
duan spot <brandennjonge@gmail.com>
2023-09-06 02:33:14 +0300

    completed assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,23 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+badge_maker("Arel")
 
 def batch_badge_creator(names):
-    return None
+
+    new_item=[f"Hello, my name is {name}." for name in names]
+    return new_item
 
 def assign_rooms(names):
-    return None
+    new_names = []
+    index = 1
+    for name in names:
+        new_names.append(f"Hello, {name}! You'll be assigned to room {index}!")
+        index +=1
+    return new_names
 
 def printer(names):
-    return None
+      for name in batch_badge_creator(names):
+        print(name)
+       
+      for name in assign_rooms(names):
+        print(name)   
